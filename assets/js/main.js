f7f35ea07371a20d7fed4b9b931d13fdc3cc8d21
kiskelcsi <kiskelcsi@gmail.com>
2021-11-17 11:47:02 +0100

    Random

--- assets/js/main.js
+++ assets/js/main.js
@@ -297,6 +297,13 @@
 
 // Winerys//
 
+var cards = $(".winerycards");
+for (var i = 0; i < cards.length; i++) {
+  var target = Math.floor(Math.random() * cards.length - 1) + 1;
+  var target2 = Math.floor(Math.random() * cards.length - 1) + 1;
+  cards.eq(target).before(cards.eq(target2));
+}
+
 const hajos = [
   {
     winery: "Huber Pince",
